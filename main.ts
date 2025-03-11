b5a84604e8bebf0a969d5162bb7860d2f0713fd4
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-03-11 13:24:14 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+
+
+//% color="#88af92"
+namespace pictureFonts {
+let fontChars = ""
+let cH = 0
+let cW = 0
+let tempImage: Image = null
+let currentFont: Image[] = []
+currentFont = []
+//% block="draw font text $text x $X y $y background $colorBG foreground $colorFG output image $output"
+//% inLineInputMode=inline
+export function drawFont (text: string, X: number, Y: number, colorBG: number, colorFG: number, output: Image) {
+    tempImage = image.create(cW * text.length, cH)
+    for (let index = 0; index <= text.length - 1; index++) {
+        spriteutils.drawTransparentImage(currentFont[fontChars.indexOf(text.charAt(index))], tempImage, index * 8, 0)
+    }
+    tempImage.replace(1, colorFG)
+    tempImage.replace(15, colorBG)
+    spriteutils.drawTransparentImage(tempImage, output, X, Y)
+}
+//% block="set font image $image char width $charWidth height $charHeight image width $imageWidth image height $imageHeight font text $fontText"
+//% inLineInputMode=inline
+export function importFont (fontImg: Image, charWidth: number, charHeight: number, imageWidth: number, imageHeight: number, fontText: string) {
+    fontChars = fontText
+    cW = charWidth
+    cH = charHeight
+    for (let y = 0; y <= imageHeight / charHeight - 1; y++) {
+        for (let x = 0; x <= imageWidth / charWidth - 1; x++) {
+            currentFont.push(image.create(charWidth, charHeight))
+            advancedBlocks.blit(currentFont[currentFont.length - 1], 0, 0, charWidth, charHeight, fontImg, x * charWidth, y * charHeight, charWidth, charHeight)
+        }
+    }
+}
+
+}
